14662986b8c26987efe6b846361c66ddb5891106
baptiste geffroy <baptistegeffroy@macbook-pro-de-baptiste.home>
2022-07-26 16:03:54 +0200

    Create: gestion of logOut

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React, { useContext } from 'react'
 import {Link} from 'react-router-dom'
 import { UserContext } from "../context/userContext";
+import {signOut} from "firebase/auth"
+import { useNavigate } from 'react-router-dom'
+import {auth} from "../firebase-config"
 
 export default function Navbar() {
+
   const { modalState, toggleModals } = useContext(UserContext);
+
+  const navigate = useNavigate()
+
+  const logOut = async () => {
+    try {
+      await signOut(auth)
+      navigate("/")
+    } catch {
+      alert("For some reasons we can't deconnect, please check your internet connexion and retry.")
+    }
+  }
 
   return (
     <nav className="navbar navbar-light bg-light px-4">
@@ -22,6 +37,7 @@ export default function Navbar() {
           Sign In
         </button>
         <button
+          onClick={logOut}
           className="btn btn-danger ms-2">
           Log Out
         </button>
